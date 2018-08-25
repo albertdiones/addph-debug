e3268c45a9ab3f5f0e1adffd09d39049784c443f
albert.diones <albert.diones@lamudi.com.ph>
2018-08-25 20:58:47 +0800

    Base class for testing

---
+++ tests/base.class.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace addph\debug\test;
+
+use PHPUnit\Framework\TestCase;
+use addph\debug\debug;
+
+ABSTRACT CLASS base EXTENDS TestCase {
+   public function setUp() {
+
+      debug::set_config(
+         (object) array(
+            'root_dir' => realpath('./'),
+
+            'developer_ips'      => array(
+            ),
+            'developer_emails'   => array('albert@add.ph','albertdiones@gmail.com'),
+            'content_type' => 'text/plain'
+         )
+      );
+   }
+
+   public function tearDown() {
+
+   }
+}
--- tests/debug_test.php
+++ tests/debug_test.php
@@ -1,31 +1,10 @@
 <?php
-use PHPUnit\Framework\TestCase;
-require_once 'src/debug.class.php';
+
+require_once 'init.php';
 use addph\debug\debug;
 
-class debug_test extends TestCase
+class debug_test extends \addph\debug\test\base
 {
-
-   static $app_config;
-
-   public function setUp() {
-
-      debug::set_config(
-         (object) array(
-            'root_dir' => realpath('./'),
-
-            'developer_ips'      => array(
-            ),
-            'developer_emails'   => array('albert@add.ph','albertdiones@gmail.com'),
-            'content_type' => 'text/plain'
-         )
-      );
-   }
-
-   public function tearDown() {
-
-   }
-
 
    /**
     * @test
